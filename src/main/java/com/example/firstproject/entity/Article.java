2c63e88adbe3cacfc0850c35e721cb6771f11443
SUBIN\user <suuu0719@gmail.com>
2024-01-06 19:12:29 +0900

    3.3 DTO를 데이터베이스에 저장하기

---
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,34 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Article {
+    @Id //엔티티의 대푯값
+    @GeneratedValue //대푯값 자동으로 생성하게 함 (제목, 내용 같은 것이 있더라도 대푯값 id로 다른 글임을 구분 가능)
+    private Long Id;
+    @Column //db에서 인식할 수 있도록 함, 두 필드가 DB테이블의 각 열과 연결됨
+    private String title;
+    @Column
+    private String content;
+
+    // Article 생성자 추가
+    public Article(Long id, String title, String content) {
+        Id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    // toString 메서드 추가
+    @Override
+    public String toString() {
+        return "Article{" +
+                "Id=" + Id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
